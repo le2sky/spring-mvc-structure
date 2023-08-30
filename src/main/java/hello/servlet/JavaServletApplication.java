9053cc5ec6024cc53b9383c4e2fcd2ad30b159d1
le2sky <leehaneul990623@gmail.com>
2023-08-31 08:17:02 +0900

    chore : initial commit

---
+++ src/main/java/hello/servlet/JavaServletApplication.java
@@ -0,0 +1,12 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServletApplication.class, args);
+    }
+}
